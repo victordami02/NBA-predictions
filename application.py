154a7d16fdf416bcb103cfa4c84210ba77f8ab97
victordami02 <vicdamilola29@gmail.com>
2023-10-23 12:21:03 +0100

    deployment

---
+++ application.py
@@ -0,0 +1,43 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipepline.predict_pipeline import CustomData, PredictPipeline
+
+application=Flask(__name__)
+
+app=application
+
+## Route for a home page
+
+@app.route('/')
+def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
+        data=CustomData(
+            elo_i=float(request.form.get('elo_i')),
+            elo_n=float(request.form.get('elo_n')),
+            win_equiv=float(request.form.get('win_equiv')),
+            opp_elo_n=float(request.form.get('opp_elo_n')),
+            forecast=float(request.form.get('forecast'))
+
+        )
+        pred_df=data.get_data_as_data_frame()
+        print(pred_df)
+        print("Before Prediction")
+
+        predict_pipeline=PredictPipeline()
+        print("Mid Prediction")
+        results=predict_pipeline.predict(pred_df)
+        print("after Prediction")
+        return render_template('home.html',results=results[0])
+    
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")        
